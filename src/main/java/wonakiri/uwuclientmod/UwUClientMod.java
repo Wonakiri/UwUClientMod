67f8dcc83b5e9f4fe50eb4dcb386bc7d17097c2a
Daniel Fuerst <daniel@puritan.kobg>
2023-02-26 19:30:20 +0100

    Initial commit

---
+++ src/main/java/wonakiri/uwuclientmod/UwUClientMod.java
@@ -0,0 +1,20 @@
+package wonakiri.uwuclientmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UwUClientMod implements ModInitializer {
+	public static final String MOD_ID = "uwu-client-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger("uwu-client-mod");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello UwU world!");
+	}
+}
---
+++ src/main/java/wonakiri/uwuclientmod/UwUModClient.java
@@ -0,0 +1,10 @@
+package wonakiri.uwuclientmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UwUModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
